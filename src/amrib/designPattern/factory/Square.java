8bce97197ebe844d96c932d09f787611504aa145
Bilal AMRI <iambilalamri@gmail.com>
2020-12-25 23:11:36 +0100

    [feat-pattern-observer]- work on patterns: factory and abstract factory

---
+++ src/amrib/designPattern/factory/Square.java
@@ -0,0 +1,11 @@
+package amrib.designPattern.factory;
+
+public class Square implements Shape {
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Draw the shape: [SQUARE] ");
+	}
+
+}
