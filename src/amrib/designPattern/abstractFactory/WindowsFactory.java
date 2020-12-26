d85ba36adaed99011b25db4ce3a34c2fa279f4e2
Bilal AMRI <iambilalamri@gmail.com>
2020-12-26 02:08:06 +0100

    [feat-pattern-observer]- work on patterns: factory and abstract factory

--- src/amrib/designPattern/abstractFactory/WindowsFactory.java
+++ src/amrib/designPattern/abstractFactory/WindowsFactory.java
@@ -15,4 +15,10 @@ public class WindowsFactory implements GUIFactory {
     public Checkbox createCheckbox() {
         return new WindowsCheckbox();
     }
+
+	@Override
+	public Label createLabel() {
+		// TODO Auto-generated method stub
+		return new WindowsLabel();
+	}
 }
