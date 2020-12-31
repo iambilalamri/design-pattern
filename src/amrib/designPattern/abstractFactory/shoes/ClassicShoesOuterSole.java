2bfd01d6dbd5d1ba384294b87e0f83e0a61d16a1
Bilal AMRI <iambilalamri@gmail.com>
2020-12-31 19:39:42 +0100

    [feat-pattern-observer]- add an other example about abstract factory pattern$

---
+++ src/amrib/designPattern/abstractFactory/shoes/ClassicShoesOuterSole.java
@@ -0,0 +1,23 @@
+package amrib.designPattern.abstractFactory.shoes;
+
+public class ClassicShoesOuterSole implements OuterSole {
+
+	@Override
+	public void design() {
+		// TODO Auto-generated method stub
+		System.out.println("Designing outerSole of [classic] shoes");
+	}
+
+	@Override
+	public void manufacture() {
+		// TODO Auto-generated method stub
+		System.out.println("Manufacturing outerSole of [classic] shoes");
+	}
+
+	@Override
+	public void testing() {
+		// TODO Auto-generated method stub
+		System.out.println("Testing outerSole of [classic] shoes");
+	}
+
+}
---
+++ src/amrib/designPattern/abstractFactory/shoes/SportShoesSide.java
@@ -0,0 +1,29 @@
+package amrib.designPattern.abstractFactory.shoes;
+
+public class SportShoesSide implements Side {
+
+	@Override
+	public void conceive() {
+		// TODO Auto-generated method stub
+		System.out.println("Conceiving side of sport shoes");
+	}
+
+	@Override
+	public void design() {
+		// TODO Auto-generated method stub
+		System.out.println("Designing side of sport shoes");
+	}
+
+	@Override
+	public void manufacture() {
+		// TODO Auto-generated method stub
+		System.out.println("Manufacturing side of sport shoes");
+	}
+
+	@Override
+	public void testing() {
+		// TODO Auto-generated method stub
+		System.out.println("Testing side of sport shoes");
+	}
+
+}
